8fb2e6ae2c20f231f80e7ddf81fd25130fa97a2c
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-27 17:07:35 +0200

    creata struttura

--- snack5/script.js
+++ snack5/script.js
@@ -1,3 +1,9 @@
 // Crea due array che hanno un numero di elementi diversi.
 // Aggiungi elementi casuali all’array che ha meno elementi,
 // fino a quando ne avrà tanti quanti l’altro.
+
+
+var primaLista =[1, 2, 3, 4, 5, 6, 7, 8, 9];
+var secondaLista=[88 ,37 ,96];
+
+var numeriRandom=

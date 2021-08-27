cfd6b9a5c7b6eec118947943d38e9c7cec9ee7cb
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-27 17:42:52 +0200

    aggiunto ciclo verifica lunghezza

--- snack5/script.js
+++ snack5/script.js
@@ -6,4 +6,16 @@
 var primaLista =[1, 2, 3, 4, 5, 6, 7, 8, 9];
 var secondaLista=[88 ,37 ,96];
 
-var numeriRandom=
+var numeriRandom=[345, 545,114,994,361,126];
+var i=0;
+while(primaLista.length< secondaLista.length){
+    primaLista.push(numeriRandom[i]);
+    i++;
+}
+while( secondaLista.length<primaLista.length){
+    secondaLista.push(numeriRandom[i]);  
+    i++;
+}
+
+console.log(primaLista);
+console.log(secondaLista);

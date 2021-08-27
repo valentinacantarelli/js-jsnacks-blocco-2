10b01a8b593a48620ef58cb0bc10d678a6833b64
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-27 15:23:04 +0200

    creata struttura

---
+++ snack4/script.js
@@ -0,0 +1,10 @@
+// Crea un array di numeri interi
+// e fai la somma di tutti gli elementi che sono in posizione (indice dell'array) dispari
+
+var numeri =[20, 17, 2688, 2552, 1147, 94, 39];
+
+var i=0;
+if (numeri % 2 == 1){
+    var numeriPosizioneDispari = numeri[i] ;
+}
+console.log(numeriPosizioneDispari);

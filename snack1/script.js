5aa0888bc9567ed1815f3166566dd14dda6d39f8
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-27 11:38:20 +0200

    creata struttura

---
+++ snack1/script.js
@@ -0,0 +1,10 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while.
+var numeroUtente;
+for(i=0; i<5;i++){
+    var numeroUtente=parseInt(prompt("Ciao,inserisci un numero"));
+}
+
+var somma=numeroUtente[i];
+console.log(somma);

cb513f9b5eddb432629241970d356e5d4c425cce
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-27 12:57:34 +0200

    creata struttura

---
+++ snack3/script.js
@@ -0,0 +1,5 @@
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
+// lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+var Nomi = ["Olivia","Emma";"Amelia";"Mia"];
+var cognomi = ["Smith", "Taylor","Wilson","Brown"];

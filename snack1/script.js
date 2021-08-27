c51cf4a666d0b91ebed86e8e15acc3f8b681c479
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-27 12:14:26 +0200

    while

--- snack1/script.js
+++ snack1/script.js
@@ -1,13 +1,22 @@
 // Il software deve chiedere per 5 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 // Esegui questo programma in due versioni, con il for e con il while.
-var numeroUtente;
+
+//for
 var somma=0;
 for(i=0; i<5;i++){
     var numeroUtente=parseInt(prompt("Ciao,inserisci un numero"));
     somma+=numeroUtente;
 }
+console.log(somma);
 
 
-
-
+//while
+var somma=0;
+var i=0;
+while(i<5){
+    var numeroUtente=parseInt(prompt("Ciao,inserisci un numero"));
+    somma+=numeroUtente;
+    i++;
+}
+console.log(somma);

8639141734c01dea2c63d2419186f8cfb2d5a4ce
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-27 16:20:20 +0200

    somma corretta

--- snack4/script.js
+++ snack4/script.js
@@ -1,17 +1,16 @@
 // Crea un array di numeri interi
 // e fai la somma di tutti gli elementi che sono in posizione (indice dell'array) dispari
 
-var numeri =[20, 17, 2688, 2552, 1147, 94, 39];
-var numeriPosizioneDispari=[];
+var numeri =[1, 2, 3, 4, 5, 6, 7];
+
 var somma=0;
 
 for (var i =0; i<numeri.length; i++ ){
     if(i %2 == 1){
-        numeriPosizioneDispari.push(numeri[i]);
-        somma+=numeriPosizioneDispari;
+        somma+=numeri[i];
     }
 }
-console.log(numeriPosizioneDispari);
+
 
 
 console.log(somma);

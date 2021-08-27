4bed6c878dc07614b0e24eda704e507a271921bf
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-27 18:25:42 +0200

    arrey uguali

--- snack5/script.js
+++ snack5/script.js
@@ -8,14 +8,48 @@ var secondaLista=[88 ,37 ,96];
 
 var numeriRandom=[345, 545,114,994,361,126];
 var i=0;
-while(primaLista.length< secondaLista.length){
-    primaLista.push(numeriRandom[i]);
-    i++;
-}
-while( secondaLista.length<primaLista.length){
-    secondaLista.push(numeriRandom[i]);  
-    i++;
-}
+ while(primaLista.length< secondaLista.length){
+     primaLista.push(numeriRandom[i]);
+     i++;
 
-console.log(primaLista);
-console.log(secondaLista);
+ }
+ while( secondaLista.length<primaLista.length){
+         secondaLista.push(numeriRandom[i]); 
+     i++;
+ }
+
+ console.log(primaLista);
+ console.log(secondaLista);
+///////////////con math random
+// var randomNumber;
+//  while(primaLista.length< secondaLista.length){
+//         randomNumber = Math.floor( Math.random() * 100);
+//      primaLista.push(randomNumber);
+
+
+// }
+//  while( secondaLista.length<primaLista.length){
+//     randomNumber = Math.floor( Math.random() * 100);
+//        secondaLista.push(randomNumber); 
+
+// }
+
+// console.log(primaLista);
+//  console.log(secondaLista);
+
+
+///////con if
+// if (primaLista.length< secondaLista.length){
+//     while(primaLista.length != secondaLista.length){
+//         primaLista.push(numeriRandom[i]);
+//         i++; }   
+    
+// }else if(primaLista.length> secondaLista.length){
+//     while( secondaLista.length!=primaLista.length){
+//         secondaLista.push(numeriRandom[i]); 
+//     i++;}
+// }else{
+//     console.log("le liste hanno la stessa lunghezza");
+// }
+// console.log(primaLista);
+// console.log(secondaLista);

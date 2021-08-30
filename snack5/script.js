9b40782fdc990d48ed78046ad4d3fe878ad26a25
valentina cantarelli <valentinacanterina@gmail.com>
2021-08-30 09:14:51 +0200

    aggiornato math random

--- snack5/script.js
+++ snack5/script.js
@@ -6,36 +6,21 @@
 var primaLista =[1, 2, 3, 4, 5, 6, 7, 8, 9];
 var secondaLista=[88 ,37 ,96];
 
-var numeriRandom=[345, 545,114,994,361,126];
-var i=0;
- while(primaLista.length< secondaLista.length){
-     primaLista.push(numeriRandom[i]);
-     i++;
+ var randomNumber;
+  while(primaLista.length< secondaLista.length){
+         randomNumber = Math.floor( Math.random() * 100);
+      primaLista.push(randomNumber);
+
 
  }
- while( secondaLista.length<primaLista.length){
-         secondaLista.push(numeriRandom[i]); 
-     i++;
+  while( secondaLista.length<primaLista.length){
+     randomNumber = Math.floor( Math.random() * 100);
+        secondaLista.push(randomNumber); 
+
  }
 
  console.log(primaLista);
- console.log(secondaLista);
-///////////////con math random
-// var randomNumber;
-//  while(primaLista.length< secondaLista.length){
-//         randomNumber = Math.floor( Math.random() * 100);
-//      primaLista.push(randomNumber);
-
-
-// }
-//  while( secondaLista.length<primaLista.length){
-//     randomNumber = Math.floor( Math.random() * 100);
-//        secondaLista.push(randomNumber); 
-
-// }
-
-// console.log(primaLista);
-//  console.log(secondaLista);
+  console.log(secondaLista);
 
 
 ///////con if
@@ -52,4 +37,4 @@ var i=0;
 //     console.log("le liste hanno la stessa lunghezza");
 // }
 // console.log(primaLista);
-// console.log(secondaLista);
+// console.log(secondaLista)
